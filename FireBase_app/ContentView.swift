ac507c7b0c3486186214576e5f0cc78b4aab760d
lorenzofravolini <lorenzofravolini@outlook.it>
2023-12-20 16:56:23 +0200

    Initial Commit

---
+++ FireBase_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FireBase_app
+//
+//  Created by Lorenzo Fravolini on 20/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FireBase_app/FireBase_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBase_appApp.swift
+//  FireBase_app
+//
+//  Created by Lorenzo Fravolini on 20/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FireBase_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
